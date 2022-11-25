13ac874f7e339b98c3d5a8f2ba1874b759ebdf7b
woefulworm <116232353+woefulworm@users.noreply.github.com>
2022-11-25 17:26:58 -0500

    init with images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import backpackData from "./backpack.json"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        The Contents of My Backpack: 11/25/22, 3:50 PM
       </header>
+      <div className='center-this'>
+        <div className='items'>
+          {backpackData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+                <div className="item">
+                  <h1>{item.name}</h1>
+                  <img src={item.image}/>
+                  <p>{item.description}</p>
+                </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
